9236c1a74428014006ff0ad1875da332fa001d8a
Grace Mtawali <gmtawal@gmail.com>
2025-10-22 10:53:41 -0700

    modified db connection code and linting

--- pet_adoption/app/models.py
+++ pet_adoption/app/models.py
@@ -2,13 +2,15 @@ from dotenv import load_dotenv
 import os
 from pymongo import MongoClient
 
-load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
+load_dotenv(os.path.join(os.path.dirname(__file__), 'CS467',  '.env'))
 
 def get_project():
     db_username = os.getenv('MONGO_USERNAME')
     db_password = os.getenv('MONGO_PASSWORD')
-    
-    mongo_uri = f"mongodb+srv://{db_username}:{db_password}@cluster0.okjtuan.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    db_name = os.getenv('MONGO_DB_NAME')
+
+    mongo_uri = f"mongodb+srv://{db_username}:{db_password}@cluster0.okjtuan.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tlsAllowInvalidCertificates=true"
+
     client = MongoClient(mongo_uri)
-    
-    return client
+    db = client[db_name]
+    return db
