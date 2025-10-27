753eef480e3c3c6cbb7831c4334e9403c3a4701e
ligonj <ligonj@oregonstate.edu>
2025-10-27 16:13:25 -0600

    added comments

--- pet_adoption/app/auth/routes.py
+++ pet_adoption/app/auth/routes.py
@@ -1,10 +1,12 @@
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # Get login information from user
     form = LoginForm()
     if form.validate_on_submit():
         email = form.email.data
         password = form.password.data
         entered_login = {'email': email, 'password': password}
+        # Check database for login credentials
         if users_collection.find_one(entered_login):
             flash("Log in successful.")
             return render_template('index.html')
@@ -12,9 +14,10 @@ def login():
             flash("User not found. Please try again or create an account.")
             return redirect('/login')
     return render_template('login.html')
-    
+
 @app.route('/register', methods=['GET', 'POST'])
 def createUser():
+    # Get account information from user
     form = RegisterForm()
     if form.validate_on_submit():
         admin_access = form.admin_access.data
@@ -23,9 +26,11 @@ def createUser():
         email = form.email.data
         password = form.password.data
         user = {"admin_access": admin_access, "first_name": first_name, "last_name": last_name, "email": email, "password": password}
+        # Check that user info doesn't already exist in DB
         if users_collection.find_one(user):
             flash("User already exists.")
             return render_template('register.html')
+        # Create user account in DB
         else:
             users_collection.insert_one(user)
             flash("User created successfully. Please log in.")
