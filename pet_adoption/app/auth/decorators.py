32d0643dec0e6e0b653f419fc0446f370ae10ea6
ligonj <ligonj@oregonstate.edu>
2025-10-27 17:32:53 -0600

    added comment

--- pet_adoption/app/auth/decorators.py
+++ pet_adoption/app/auth/decorators.py
@@ -4,6 +4,7 @@ from flask import redirect, url_for, session, flash
 def admin_access(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
+        # Check session for admin_access T/F value
         if not session.get('admin_access'):
             flash("This page requires admin access.")
             return redirect("/login")
