5e4da17652597d13e60ab0e6d70a91b84b23163b
ligonj <ligonj@oregonstate.edu>
2025-10-27 17:26:48 -0600

    created admin_access decorator

---
+++ pet_adoption/app/auth/decorators.py
@@ -0,0 +1,11 @@
+from functools import wraps
+from flask import redirect, url_for, session, flash
+
+def admin_access(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('admin_access'):
+            flash("This page requires admin access.")
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
