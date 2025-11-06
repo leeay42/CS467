1ef1f9e1337fadbb114bb0a3e42fc4516b4dcba4
Ah Young Lee <122363470+leeay42@users.noreply.github.com>
2025-11-06 14:08:06 -0800

    Update __init__.py
    placed mongodb connection within create_app function.  Added in app.config for animals and users so routes have access to db. Added in main.routes for blueprint

--- pet_adoption/app/__init__.py
+++ pet_adoption/app/__init__.py
@@ -6,24 +6,31 @@ from pymongo import MongoClient
 # Load environment variables
 load_dotenv()
 
-# MongoDB connection
-db_username = os.getenv('MONGO_USERNAME')
-db_password = os.getenv('MONGO_PASSWORD')
-db_name = os.getenv('MONGO_DB_NAME')
-
-mongo_uri = f"mongodb+srv://{db_username}:{db_password}@cluster0.okjtuan.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tlsAllowInvalidCertificates=true"
-
-client = MongoClient(mongo_uri)
-db = client[db_name]
-
-
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
+    # MongoDB connection
+    db_username = os.getenv('MONGO_USERNAME')
+    db_password = os.getenv('MONGO_PASSWORD')
+    db_name = os.getenv('MONGO_DB_NAME')
+
+    mongo_uri = f"mongodb+srv://{db_username}:{db_password}@cluster0.okjtuan.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tlsAllowInvalidCertificates=true"
+
+    client = MongoClient(mongo_uri)
+    db = client[db_name]
+
+    # Store database in app config for blueprints to access
+    app.config['animals_collection'] = db['animals']
+    app.config['users_collection'] = db['users']
+
     # Register blueprints
     from app.admin.routes import admin
     app.register_blueprint(admin, url_prefix='/admin')
+
+    # Register main blueprints
+    from app.main.routes import main
+    app.register_blueprint(main)
 
     return app
 
