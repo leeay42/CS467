d3ef44a152bcddef3d213605527c3cfc9b4b6ba1
Grace Mtawali <gmtawal@gmail.com>
2025-11-14 14:06:23 -0800

    clean up references

--- pet_adoption/app/static/js/modal.js
+++ pet_adoption/app/static/js/modal.js
@@ -1,3 +1,10 @@
+/*
+  References:
+  - https://www.w3schools.com/howto/howto_css_modals.asp
+  - https://developer.mozilla.org/en-US/docs/Web/API/Window/onclick
+  - https://sabe.io/tutorials/how-to-create-modal-popup-box
+*/
+
 function openCreateModal() {
     document.getElementById("modalTitle").innerText = "Add New Pet";
     document.getElementById("petForm").action = "{{ url_for('admin.admin_dashboard') }}";
