940414a61cc64d0af5d547a6d3ffbc9bd87c6ce8
Grace Mtawali <gmtawal@gmail.com>
2025-11-21 12:27:08 -0800

    revert deploy changes

--- pet_adoption/run.py
+++ pet_adoption/run.py
@@ -1,6 +1,7 @@
-from pet_adoption.app import create_app
+from app import create_app
 
 app = create_app()
+app.run(host='127.0.0.1', port=5001, debug=True)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5001, debug=True)
+    app.run(debug=True)
